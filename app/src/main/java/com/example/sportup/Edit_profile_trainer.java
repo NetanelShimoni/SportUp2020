4047dc7fe770a3d0ff81c6364404b10af3551625
or2209 <orlaharty1@gmail.com>
2020-12-28 20:38:25 +0200

    check

--- app/src/main/java/com/example/sportup/Edit_profile_trainer.java
+++ app/src/main/java/com/example/sportup/Edit_profile_trainer.java
@@ -16,7 +16,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-///ffoooorrkkkkkkkkkkkkkk
+//child
 public class Edit_profile_trainer extends AppCompatActivity {
     TextView welcome;
     Intent intent;
