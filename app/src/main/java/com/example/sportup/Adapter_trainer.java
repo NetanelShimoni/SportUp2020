8ec9293e61e94669409df5f3b27717b9fc77e899
Netanel Shimoni <57719538+NetanelShimoni@users.noreply.github.com>
2020-12-29 10:40:05 +0200

    Update Adapter_trainer.java

--- app/src/main/java/com/example/sportup/Adapter_trainer.java
+++ app/src/main/java/com/example/sportup/Adapter_trainer.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
-
+//fdfdfdfsdfsdfdf
 import java.util.ArrayList;
 import java.util.List;
 
